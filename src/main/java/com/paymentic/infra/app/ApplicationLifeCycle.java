0d01e4f225e474fb6c1b2b2ae3254d21f5f83ad9
claudioed <claudioed.oliveira@gmail.com>
2024-01-09 12:57:22 -0300

    add log for profile

---
+++ src/main/java/com/paymentic/infra/app/ApplicationLifeCycle.java
@@ -0,0 +1,17 @@
+package com.paymentic.infra.app;
+
+import com.paymentic.adapter.kafka.TransactionRegisteredProcessor;
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ProfileManager;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class ApplicationLifeCycle {
+  private static final Logger LOGGER = Logger.getLogger(TransactionRegisteredProcessor.class);
+  void onStart(@Observes StartupEvent ev) {
+    LOGGER.info("The application is starting with profile " + ProfileManager.getActiveProfile());
+  }
+
+}
